80ecf369b2e086a7872ae0ae76d46f10800eb2f4
AJohansen-code <aracosta@ualr.edu>
2025-04-10 19:33:06 -0500

    Update main.cpp

--- projects/project02/main.cpp
+++ projects/project02/main.cpp
@@ -1,5 +1,7 @@
 #include "checkers.h"
 #include <iostream>
+#include <limits>
+
 
 int main() {
     Checkers game;
@@ -10,9 +12,16 @@ int main() {
         std::cout << game;
         int startRow, startCol, endRow, endCol;
         std::cout << "Enter your move (startRow startCol endRow endCol): ";
-        std::cin >> startRow >> startCol >> endRow >> endCol;
-        game.play(startRow, startCol, endRow, endCol);
+
+        if (std::cin >> startRow >> startCol >> endRow >> endCol){
+           if (!game.play(startRow, startCol, endRow, endCol)){
+    } else {
+            std::cout << "Invalid input. Please enter four integers." << std::endl;
+            std::cin.clear(); 
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        }
     }
+}
 
     std::cout << game;
 
